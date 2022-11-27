a18fe09f1f978e70fb5d2f1f969e0c9258208864
ebenGunadi <egunadi@medinformatix.com>
2022-11-27 09:29:11 -0800

    Update scrub_data.py to also scrub restaurants.csv

--- scrub_data.py
+++ scrub_data.py
@@ -1,24 +1,34 @@
 import pandas as pd
 
-menu_filepath = "data/ubereats/restaurant-menus.csv"
+menus_filepath = "data/ubereats/restaurant-menus.csv"
+restaurants_filepath = "data/ubereats/restaurants.csv"
 
 # import data
-menu_df = pd.read_csv(menu_filepath, delimiter=',', encoding='utf-8')
+menus_df = pd.read_csv(menus_filepath, delimiter=',', encoding='utf-8')
+restaurants_df = pd.read_csv(restaurants_filepath, delimiter=',', encoding='utf-8')
     
-# scrub data
+# scrub menus data
 name_filter_list = ['ayam', 'ikan', 'kambing', 'sapi', 'tahu', 'telur', 'tempe', 'udang']
-menu_df = menu_df[  (menu_df['category'].str.contains('indonesia', na=False, case=False)) |
-                    (menu_df['name']
+menus_df = menus_df[  (menus_df['category'].str.contains('indonesia', na=False, case=False)) |
+                    (menus_df['name']
                         .str.contains('|'.join(name_filter_list), na=False, case=False))    ]
 
 category_scrub_list = ['sake', 'medicines & treatments', 'bedding', 'storage & cleaning', 'home decor', 'wine', 'nursing & feeding', 'bookstore', 'retail']
-menu_df = menu_df[menu_df['category']
+menus_df = menus_df[menus_df['category']
                         .str.contains('|'.join(category_scrub_list), na=False, case=False) 
                             == False]
                             
 name_scrub_list = ['gekkeikan', 'ramayama', 'mudang', 'nuegados', 'nayama', 'bayamon', 'quesapizza', 'quesapita', 'mikan', 'ketchikan', 'haikan', 'ayamashe', 'fishikan', 'pallipalayam', 'tempered', 'temper', 'tempest', 'ayamase', 'shikanjavi', 'nuegados', 'bikaneri', 'kottayam', 'sapitos']
-menu_df = menu_df[menu_df['name']
+menus_df = menus_df[menus_df['name']
                         .str.contains('|'.join(name_scrub_list), na=False, case=False) 
                             == False]
+
+# scrub restaurants data
+category_scrub_list = ['bookstore', 'personal care', 'mercado express']
+restaurants_df = restaurants_df[restaurants_df['category']
+                        .str.contains('|'.join(category_scrub_list), na=False, case=False) 
+                            == False]
+
 # export data
-menu_df.to_csv('data/ubereats/restaurant-menus-abridged.csv', encoding='utf-8', index=False)
+menus_df.to_csv('data/ubereats/restaurant-menus-abridged.csv', encoding='utf-8', index=False)
+restaurants_df.to_csv('data/ubereats/restaurants-abridged.csv', encoding='utf-8', index=False)
