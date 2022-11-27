2e0e1b9fbde506a9c9b0f67b0c2f2ffc9de3da81
ebenGunadi <egunadi@medinformatix.com>
2022-11-27 09:00:00 -0800

    In scrub_data.py, preserve all menu items with a category containing "indonesia"

--- scrub_data.py
+++ scrub_data.py
@@ -7,15 +7,16 @@ menu_df = pd.read_csv(menu_filepath, delimiter=',', encoding='utf-8')
     
 # scrub data
 name_filter_list = ['ayam', 'ikan', 'kambing', 'sapi', 'tahu', 'telur', 'tempe', 'udang']
-menu_df = menu_df[menu_df['name']
-                        .str.contains('|'.join(name_filter_list), na=False, case=False)]
+menu_df = menu_df[  (menu_df['category'].str.contains('indonesia', na=False, case=False)) |
+                    (menu_df['name']
+                        .str.contains('|'.join(name_filter_list), na=False, case=False))    ]
 
 category_scrub_list = ['sake', 'medicines & treatments', 'bedding', 'storage & cleaning', 'home decor', 'wine', 'nursing & feeding', 'bookstore', 'retail']
 menu_df = menu_df[menu_df['category']
                         .str.contains('|'.join(category_scrub_list), na=False, case=False) 
                             == False]
                             
-name_scrub_list = ['gekkeikan', 'ramayama', 'mudang', 'nuegados', 'nayama', 'bayamon', 'quesapizza', 'quesapita', 'mikan', 'ketchikan', 'haikan', 'ayamashe', 'fishikan', 'pallipalayam', 'tempered', 'temper', 'tempest', 'ayamase', 'shikanjavi', 'nuegados', 'bikaneri', 'kottaya', 'sapitos']
+name_scrub_list = ['gekkeikan', 'ramayama', 'mudang', 'nuegados', 'nayama', 'bayamon', 'quesapizza', 'quesapita', 'mikan', 'ketchikan', 'haikan', 'ayamashe', 'fishikan', 'pallipalayam', 'tempered', 'temper', 'tempest', 'ayamase', 'shikanjavi', 'nuegados', 'bikaneri', 'kottayam', 'sapitos']
 menu_df = menu_df[menu_df['name']
                         .str.contains('|'.join(name_scrub_list), na=False, case=False) 
                             == False]
