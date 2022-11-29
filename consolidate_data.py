a31c4b117255f03ebf9e569a0f6fb51fcd287a60
ebenGunadi <egunadi@medinformatix.com>
2022-11-28 17:36:14 -0800

    Have consolidate_data.py also put together data/indorestaurants.csv

--- consolidate_data.py
+++ consolidate_data.py
@@ -9,6 +9,9 @@ telur_filepath = "data/indorecipes/dataset-telur.csv"
 tempe_filepath = "data/indorecipes/dataset-tempe.csv"
 udang_filepath = "data/indorecipes/dataset-udang.csv"
 
+abridged_menus_filepath = "data/ubereats/restaurant-menus-abridged.csv"
+abridged_restaurants_filepath = "data/ubereats/restaurants-abridged.csv"
+
 # import data
 ayam_df = pd.read_csv(ayam_filepath, delimiter=',', encoding='utf-8')
 ikan_df = pd.read_csv(ikan_filepath, delimiter=',', encoding='utf-8')
@@ -19,7 +22,10 @@ telur_df = pd.read_csv(telur_filepath, delimiter=',', encoding='utf-8')
 tempe_df = pd.read_csv(tempe_filepath, delimiter=',', encoding='utf-8')
 udang_df = pd.read_csv(udang_filepath, delimiter=',', encoding='utf-8')
 
-# preserve data on source file
+menus_df = pd.read_csv(abridged_menus_filepath, delimiter=',', encoding='utf-8')
+restaurants_df = pd.read_csv(abridged_restaurants_filepath, delimiter=',', encoding='utf-8')
+
+# for indorecipes, preserve data on source file
 ayam_df['broadcategory'] = 'ayam'
 ikan_df['broadcategory'] = 'ikan'
 kambing_df['broadcategory'] = 'kambing'
@@ -32,5 +38,10 @@ udang_df['broadcategory'] = 'udang'
 # consolidate data
 indofoods_df =  pd.concat([ayam_df, ikan_df, kambing_df, sapi_df, tahu_df, telur_df, tempe_df, udang_df])
 
+indorestaurants_df = pd.merge(  menus_df, restaurants_df, 
+                                left_on='restaurant_id', right_on='id', 
+                                suffixes=('_menuitem', '_restaurant')   )
+
 # export data
-indofoods_df.to_csv('data/indorecipes/indofoods.csv', encoding='utf-8', index=False)
+indofoods_df.to_csv('data/indofoods.csv', encoding='utf-8', index=False)
+indorestaurants_df.to_csv('data/indorestaurants.csv', encoding='utf-8', index=False)
